0d2e2beb69d3fe2b7f3b46e9a8ef6dcc990ecdc5
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-04 10:56:26 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [showForm, setShowForm] = useState<boolean>(false);
+
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
