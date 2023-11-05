e63fed274cf0e4958a8507ad132b72e1b759dceb
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-04 20:01:32 -0500

    made banner and header mobile friendly, work in progress

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Banner() {
   return (
-    <div>
-      <img src="/src/assets/up-47.jpg" alt="food image" />
-      <img src="/src/assets/Aba_VillageSalad.jpg" alt="salad" />
+    <div className={`hidden md:flex md:flex-row md:w-full md:h-72`}>
+      <img className={`w-7/12`} src="/src/assets/up-47.jpg" alt="food image" />
+      <img className={`w-4/12`} src="/src/assets/Aba_VillageSalad.jpg" alt="salad" />
     </div>
   )
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
 export default function Header() {
-  return (
-    <header className={`flex flex-col flex-center mb-4`}>
-        <img className={`p-16`} src="/src/assets/aba-logo.svg" alt="aba-logo" />
+    return (
+        <header className={`flex flex-col-reverse md:flex-row flex-center mb-4 md:p-8 items-center`}>
+            <img className={`hidden md:block w-48 h-48`} src="/src/assets/aba-logo.svg" alt="aba-logo" />
+            <img className={`block md:hidden backdrop-brightness-90 p-24 bg-plates bg-cover bg-center bg-no-repeat bg-local`} src="/src/assets/aba-logo-cream.svg" alt="aba-logo" />
+            
 
-        <nav className={`px-4`}>
-            <ul className={`py-2 border-t border-b border-black flex flex-wrap justify-between`}>
-                <li>Reserve</li>
-                <li>Menu</li>
-                <li>Group Dining</li>
-                <li>Events</li>
-                <li>Team</li>
-            </ul>
-        </nav>
-    </header>
-  )
+            <nav className={`px-4 my-2 w-full`}>
+                <ul className={`py-2 md:w-4/6 md:py-4 border-t border-b border-black flex flex-wrap justify-between md:justify-around`}>
+                    <li>Reserve</li>
+                    <li>Menu</li>
+                    <li>Group Dining</li>
+                    <li>Events</li>
+                    <li>Team</li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
