bd4c034a757dc56d9eaf47433f0a7e4b2bd61e1c
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-05 17:47:24 -0600

    styled reservation form

--- src/components/reservation/Reservation.tsx
+++ src/components/reservation/Reservation.tsx
@@ -1,55 +1,108 @@
 import { useForm } from "react-hook-form"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ActionButton from "../shared-ui/ActionButton";
 
 export default function Reservation() {
     const [formStep, setFormStep] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(false);
 
     const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const submitHandler = async () => {
-
+    const submitHandler = async (formData: any) => {
+        console.log(formData)
+        // make async call to table data api and add reservation
+        // if it fails, alert user to try again (later?)
+        // if it succeeds show success message
+        // we will assume success for this example
+        setFormStep(formStep + 1);
     }
 
     const tableFinder = async () => {
+        
         // make async call to table data api
         // return available tables
-        //if no tables are available for the selected settings, alert user
+        // if no tables are available for the selected settings, alert user
+        // assume available tables
+        setFormStep(formStep + 1)
     }
 
     const times: Array<number> = [11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 23]
 
+    useEffect(() => {
+        console.log(errors)
+    }, [])
+
     return (
-        <section>
-            <form onSubmit={tableFinder}>
-                <h3>MAKE A RESERVATION</h3>
-                {/* <label htmlFor="resDate">Reservation Date</label> */}
-                <input id="resDate" type="date" {...register("date", {
-                    required: true,
-                    valueAsDate: true,
-                })} />
+        <section className="flex flex-col h-fit transition-transform duration-100 justify-center items-center border-t border-b w-11/12 border-black mx-4 py-2">
+            <h3 className="text-lg">MAKE A RESERVATION</h3>
+            <form className={`${formStep < 2 ? "block" : "hidden"} font-montserrat`} onSubmit={handleSubmit(submitHandler)}>
+                <div className={`${formStep < 1 ? "flex" : "hidden"} flex-col mb-2`}>
+                    <label className="hidden" htmlFor="resDate">Reservation Date</label>
+                    <input className={`w-56 h-10 border text-center`} id="resDate" type="date" {...register("date", {
+                        required: true,
+                        valueAsDate: true,
+                    })} />
+                    
+                    <label className="hidden" htmlFor="resTime">Reservation Time</label>
+                    <select id="resTime" className={`w-56 h-10 border text-center`} {...register("time", {
+                        required: true,
+                    })}>
+                        {times.map((time, index) => {
 
-                <select {...register("time", {
-                    required: true,
-                })}>
-                    {times.map((time, index) => {
+                            return (
+                                <option key={`${time}${index}`}value={`${time}:${index % 2 == 0 ? "00" : "30"}`}>
+                                    {`${time > 12 ? time - 12 : time}:${index % 2 == 0 ? "00" : "30"}`}
+                                </option>
+                            )
+                        })}
+                    </select>
 
-                        return (
-                            <option value={`${time}:${index % 2 == 0 ? "00" : "30"}`}>
-                                {`${time > 12 ? time - 12 : time}:${index % 2 == 0 ? "00" : "30"}`}
-                            </option>
-                        )
-                    })}
-                </select>
+                    <label className="hidden" htmlFor="numGuests">Number of Guests</label>
+                    <select id="numGuests" className={`w-56 h-10 border text-center`} {...register("guests", {
+                        required: true,
+                    })}>
+                        {[...Array(8).keys()].map(item => {
+                            return (
+                                <option key={item} value={item+1}>{item+1 >= 8 ? "8+" : item+1}</option>
+                            )
+                        })}
+                    </select>
+                    <ActionButton type="button" text="Find a table" action={tableFinder} />
+                </div>
 
-                <select id="">
-                    {[...Array(8).keys()].map(key => {
-                        return (
-                            <option value={key+1}>{key+1 >= 8 ? "8+" : key+1}</option>
-                        )
-                    })}
-                </select>
-                <button type="submit">Find a Table</button>
+                <div className={`${formStep === 1 ? "flex" : "hidden"} flex-col mb-2`}>
+                    <label htmlFor="nameInput" className="hidden"></label>
+                    <input className={`w-56 h-10 border text-center`} placeholder="Name" id="nameInput" type="text" {...register("name", {
+                        required: "Please enter your name",
+                        minLength: {
+                            value: 2,
+                            message: "Please enter your name"
+                        }
+                    })} />
+
+                    <label htmlFor="phoneInput" className="hidden"></label>
+                    <input className={`w-56 h-10 border text-center`} placeholder="Phone Number" id="phoneInput" type="text" {...register("phone", {
+                        required: true,
+                        minLength: {
+                            value: 10,
+                            message: "Please enter a valid phone number. ex. 555 555 5555"
+                        }
+                    })} />
+
+                    <label htmlFor="emailInput" className="hidden"></label>
+                    <input className={`w-56 h-10 border text-center`} placeholder="Email Address" id="emailInput" type="email" {...register("email", {
+                        required: "Please enter your email",
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "You must provide a valid email address."
+                        }
+                    })} />
+                    <ActionButton type="submit" text="Reserve my table" />
+                </div>
             </form>
+            <div className={`${formStep < 2 ? "hidden" : "block"}`}>
+                Success!
+            </div>
         </section>
     )
 }
--- src/components/shared-ui/ActionButton.tsx
+++ src/components/shared-ui/ActionButton.tsx
@@ -1,9 +1,11 @@
-export default function ActionButton({ text, action }: 
+export default function ActionButton({ text, action, type, validation=false }: 
     { text: string,
-    action: () => void }) {
+    action?: () => void,
+    type: "button" | "submit" | "reset" | undefined,
+    validation?: boolean}) {
 
     return (
-        <button className={``} onClick={action} type="button">
+        <button className={`w-56 h-10 text-center bg-blue-400 `} onClick={action ? action : () => {console.log("")}} type={type} disabled={validation}>
             {text}
         </button>
     )
