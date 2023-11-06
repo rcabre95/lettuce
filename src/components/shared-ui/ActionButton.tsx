20b13ab3cd534aa9116370698c6876b71f228406
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-05 20:59:14 -0600

    added extraStyles prop to Action button

--- src/components/shared-ui/ActionButton.tsx
+++ src/components/shared-ui/ActionButton.tsx
@@ -1,11 +1,12 @@
-export default function ActionButton({ text, action, type, validation=false }: 
+export default function ActionButton({ text, action, type, validation=false, extraStyles }: 
     { text: string,
     action?: () => void,
     type: "button" | "submit" | "reset" | undefined,
-    validation?: boolean}) {
+    validation?: boolean,
+    extraStyles?: string}) {
 
     return (
-        <button className={`w-56 h-10 text-center bg-lettuce-blue text-lettuce-cream text-sm`} onClick={action ? action : () => {console.log("")}} type={type} disabled={validation}>
+        <button className={`w-56 h-10 text-center bg-lettuce-blue text-lettuce-cream text-sm ${extraStyles}`} onClick={action ? action : () => {console.log("")}} type={type} disabled={validation}>
             {text}
         </button>
     )
