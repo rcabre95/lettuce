31305169e03165d15d457dfc644d7cf523164c68
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-06 17:51:42 -0600

    fixed visual bugs

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className={`flex flex-col-reverse md:flex-row mb-4 md:m-8  items-center w-full md:w-2/3 justify-center`}>
+        <header className={`flex flex-col-reverse md:flex-row mb-4 md:m-8  items-center w-full md:w-11/12 md:max-w-7xl justify-center`}>
             <img className={`hidden md:block w-48 h-48`} src="/src/assets/aba-logo.svg" alt="aba-logo" />
             <img className={`block md:hidden backdrop-brightness-90 md:w-0 md:h-0 p-24 bg-plates bg-cover bg-center bg-no-repeat bg-local`} src="/src/assets/aba-logo-cream.svg" alt="aba-logo" />
             
