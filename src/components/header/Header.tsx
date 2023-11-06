f357c35ca9d0ddf4e4ecc5c69289f07c35df8914
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-06 16:55:19 -0600

    responsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
-
 export default function Header() {
     return (
-        <header className={`flex flex-col-reverse md:flex-row flex-center mb-4 md:p-8 items-center`}>
+        <header className={`flex flex-col-reverse md:flex-row mb-4 md:m-8  items-center w-full md:w-2/3 justify-center`}>
             <img className={`hidden md:block w-48 h-48`} src="/src/assets/aba-logo.svg" alt="aba-logo" />
-            <img className={`block md:hidden backdrop-brightness-90 p-24 bg-plates bg-cover bg-center bg-no-repeat bg-local`} src="/src/assets/aba-logo-cream.svg" alt="aba-logo" />
+            <img className={`block md:hidden backdrop-brightness-90 md:w-0 md:h-0 p-24 bg-plates bg-cover bg-center bg-no-repeat bg-local`} src="/src/assets/aba-logo-cream.svg" alt="aba-logo" />
             
 
-            <nav className={`px-4 my-2 w-full`}>
-                <ul className={`py-2 md:w-4/6 md:py-4 border-t border-b border-black flex flex-wrap justify-between md:justify-around`}>
-                    <li>Reserve</li>
-                    <li>Menu</li>
-                    <li>Group Dining</li>
-                    <li>Events</li>
-                    <li>Team</li>
+            <nav className={`px-4 my-2 w-full md:w-5/6`}>
+                <ul className={`py-2  w-full border-t border-b border-black flex flex-wrap justify-between md:justify-around`}>
+                    <li><a href="/">Reserve</a></li>
+                    <li><a href="/">Menu</a></li>
+                    <li><a href="/">Group Dining</a></li>
+                    <li><a href="/">Events</a></li>
+                    <li><a href="/">Team</a></li>
                 </ul>
             </nav>
         </header>
