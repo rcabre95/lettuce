cc2bf5042b9f24a828c17949b7e39055f8b13e0e
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-06 20:10:55 -0600

    made footer more responsive

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className={`bg-dark-wood bg-cover bg-center bg-no-repeat bg-local h-fit w-screen md:flex md:flex-col md:items-center md:justify-center text-lettuce-cream p-4`}>
+        <footer className={`bg-dark-wood bg-cover bg-center bg-no-repeat bg-local h-fit md:h-96 w-screen md:flex md:flex-col md:items-end md:justify-end text-lettuce-cream p-4`}>
             <UpperFooter />
             <LowerFooter />
         </footer>
@@ -11,37 +11,37 @@ function UpperFooter() {
     return (
     <>
     <div className="flex w-full md:w-2/3 flex-wrap justify-end">
-        <section className={`w-1/2 md:w-1/6 text-right md:text-left mb-4`}>
-            <h4>HOURS</h4>
+        <section className={`w-1/2 md:w-72 text-right md:text-left mb-4`}>
+            <h4 className="text-base md:text-lg">HOURS</h4>
             <ul>
-                <li className="text-sm">Tues - Thurs 12pm - 10pm</li>
-                <li className="text-sm">Fri 12pm - 1am</li>
-                <li className="text-sm">Sat 11am - 1am</li>
-                <li className="text-sm">Sun 10am - 9pm</li>
+                <li className="text-sm font-montserrat md:text-base">Tues - Thurs 12pm - 10pm</li>
+                <li className="text-sm font-montserrat md:text-base">Fri 12pm - 1am</li>
+                <li className="text-sm font-montserrat md:text-base">Sat 11am - 1am</li>
+                <li className="text-sm font-montserrat md:text-base">Sun 10am - 9pm</li>
             </ul>
         </section>
-        <section className={`w-1/2 md:w-1/6 text-right md:text-left`}>
-            <h4>LOCATIONS</h4>
-            <span className="text-sm">302 N Green Street</span><br />
-            <span className="text-sm">3rd Floor</span><br />
-            <span className="text-sm">Chicago, IL 60607</span><br />
-            <span className="text-sm">&#40;773&#41; 645-1400</span><br />
+        <section className={`w-1/2 md:w-72 text-right md:text-left`}>
+            <h4 className="text-base md:text-lg">LOCATIONS</h4>
+            <span className="text-sm font-montserrat md:text-base">302 N Green Street</span><br />
+            <span className="text-sm font-montserrat md:text-base">3rd Floor</span><br />
+            <span className="text-sm font-montserrat md:text-base">Chicago, IL 60607</span><br />
+            <span className="text-sm font-montserrat md:text-base">&#40;773&#41; 645-1400</span><br />
         </section>
         
     </div>
-        <div className="w-full md:w-2/3 mb-4 h-fit md:flex md:flex-col md:items-end mt-2 mr-2">
-            <section className="md:w-1/3 text-right md:text-left md:flex md:flex-col">
-                <a href="/"><h4 className="underline">CONTACT</h4></a>
-                <a href="/"><h4 className="underline">NEWSLETTER</h4></a>
-            </section>
-        </div>
+    <div className="w-full md:w-2/3 mb-4 h-fit md:flex md:flex-col md:items-end mt-2 mr-2 text-lg">
+        <section className="md:w-72 lg:mr-72 text-right md:text-left md:flex md:flex-col">
+            <a href="/"><h4 className="">CONTACT</h4></a>
+            <a href="/"><h4 className="">NEWSLETTER</h4></a>
+        </section>
+    </div>
     </>
     )
 }
 
 function LowerFooter() {
     return (
-    <nav className="w-full text-xs">
+    <nav className="w-full text-xs md:flex md:self-end -mb-3">
         <ul className="w-full h-20 md:h-10 flex flex-wrap justify-center md:items-end items-center">
             <li className="p-2"><a href="/">FREQUENT DINER</a></li>
             <li className="p-2"><a href="/">GIFT CARDS</a></li>
