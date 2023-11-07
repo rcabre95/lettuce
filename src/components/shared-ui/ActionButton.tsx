77e9905afe924ece76e7c7ee03ca67ff16cf10d1
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-06 19:52:53 -0600

    added options to action button

--- src/components/shared-ui/ActionButton.tsx
+++ src/components/shared-ui/ActionButton.tsx
@@ -1,12 +1,14 @@
-export default function ActionButton({ text, action, type, validation=false, extraStyles }: 
+export default function ActionButton({ text, action, type, validation=false, width="w-56", height="h-10", extraStyles }: 
     { text: string,
     action?: () => void,
     type: "button" | "submit" | "reset" | undefined,
     validation?: boolean,
-    extraStyles?: string}) {
+    extraStyles?: string,
+    width?: string,
+    height?: string}) {
 
     return (
-        <button className={`w-56 h-10 text-center bg-lettuce-blue text-lettuce-cream text-sm ${extraStyles}`} onClick={action ? action : () => {console.log("")}} type={type} disabled={validation}>
+        <button className={`${width} ${height} text-center bg-lettuce-blue text-lettuce-cream ${extraStyles}`} onClick={action ? action : () => {console.log("")}} type={type} disabled={validation}>
             {text}
         </button>
     )
