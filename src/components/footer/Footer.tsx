1468f66acf449048491bd2a8bcaa128fda476cd3
rcabre95 <raphael.cabrera111@gmail.com>
2023-11-06 18:47:04 -0600

    made footer responsive

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className={`bg-dark-wood bg-cover bg-center bg-no-repeat bg-local h-fit w-screen text-lettuce-cream p-4`}>
+        <footer className={`bg-dark-wood bg-cover bg-center bg-no-repeat bg-local h-fit w-screen md:flex md:flex-col md:items-center md:justify-center text-lettuce-cream p-4`}>
             <UpperFooter />
             <LowerFooter />
         </footer>
@@ -9,8 +9,9 @@ export default function Footer() {
 
 function UpperFooter() {
     return (
-    <div className="flex flex-wrap mb-4">
-        <section className={`w-1/2 text-right mb-4`}>
+    <>
+    <div className="flex w-full md:w-2/3 flex-wrap bg-blue-950 justify-end">
+        <section className={`w-1/2 md:w-1/6 text-right md:text-left mb-4`}>
             <h4>HOURS</h4>
             <ul>
                 <li className="text-sm">Tues - Thurs 12pm - 10pm</li>
@@ -19,25 +20,29 @@ function UpperFooter() {
                 <li className="text-sm">Sun 10am - 9pm</li>
             </ul>
         </section>
-        <section className={`w-1/2 text-right`}>
+        <section className={`w-1/2 md:w-1/6 text-right md:text-left`}>
             <h4>LOCATIONS</h4>
             <span className="text-sm">302 N Green Street</span><br />
             <span className="text-sm">3rd Floor</span><br />
             <span className="text-sm">Chicago, IL 60607</span><br />
             <span className="text-sm">&#40;773&#41; 645-1400</span><br />
         </section>
-        <section className="w-full h-fit text-right mt-2">
-            <a href="/"><h4 className="underline">CONTACT</h4></a>
-            <a href="/"><h4 className="underline">NEWSLETTER</h4></a>
-        </section>
+        
     </div>
+        <div className="w-full md:w-2/3 mb-4 bg-green-300 h-fit md:flex md:flex-col md:items-end mt-2 mr-2">
+            <section className="md:w-1/3 bg-slate-400 text-right md:text-left md:flex md:flex-col">
+                <a href="/"><h4 className="underline">CONTACT</h4></a>
+                <a href="/"><h4 className="underline">NEWSLETTER</h4></a>
+            </section>
+        </div>
+    </>
     )
 }
 
 function LowerFooter() {
     return (
-    <nav className="w-full text-xs ">
-        <ul className="w-full h-20 flex flex-wrap justify-center items-center">
+    <nav className="w-full text-xs">
+        <ul className="w-full h-20 md:h-10 flex flex-wrap justify-center md:items-end items-center">
             <li className="p-2"><a href="/">FREQUENT DINER</a></li>
             <li className="p-2"><a href="/">GIFT CARDS</a></li>
             <li className="p-2"><a href="/">EMPLOYMENT</a></li>
